d59b87a3673475ce02ba3762ab83d105daef7a03
menshenin <fitzgeraldfox@yandex.ru>
2024-11-20 14:28:25 +0300

    tree node fix

--- sql/tree-node.sql
+++ sql/tree-node.sql
@@ -4,7 +4,7 @@
 select
     t.id,
     case 
-        when t.p_id = null then 'Root'
+        when t.p_id IS NULL then 'Root'
         when t.id in (select t.id from Tree t where t.id in (select distinct p_id from Tree)) then 'Inner'
         else 'Leaf'
     end as type
